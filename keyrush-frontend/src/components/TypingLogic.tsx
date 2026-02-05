3ad785830bfaf4bdab1379b32f31d87d3ce16f49
Gunjan <gunjanrawat5@gmail.com>
2026-02-05 15:42:31 -0500

    added typing capturing

--- keyrush-frontend/src/components/TypingLogic.tsx
+++ keyrush-frontend/src/components/TypingLogic.tsx
@@ -1,22 +1,43 @@
 'use client'
-import React, { SetStateAction, useEffect, useState } from 'react'
+import React, { SetStateAction, useEffect, useRef, useState } from 'react'
 
 interface TimeDropdownProps { 
     time : number,
     setTime : React.Dispatch<SetStateAction<number>>,
+    PARA : string
 }
+
 const TypingLogic = ({time,setTime}:TimeDropdownProps) => {
    const [start,setStart] = useState(false)
+   const [input,setInput] = useState("")
+   const startedRef = useRef(false)
    
     useEffect(() => {
-        function handleKeyDown() {
-        setStart(true);
-    }
+        function handleKeyDown(event: KeyboardEvent) {
+            const key = event.key
+            const isPrintabale = key.length === 1
+            const isBackspace = key === 'Backspace'
+            if (time<=0) return
+            setStart(true);
+            if (!startedRef.current && (isPrintabale || isBackspace)){
+                startedRef.current = true
+                setStart(true)
+            }
+            if(!startedRef.current) return;
+
+            if(isPrintabale) {
+                setInput((prev)=>prev + key)
+            } else if (isBackspace){
+                setInput((prev)=>prev.slice(0,-1))
+            }
+        }
         window.addEventListener("keydown", handleKeyDown);
         return () => {
             window.removeEventListener("keydown", handleKeyDown);
         };
-    }, []);
+    }, [time]);
+    
+        
     useEffect(()=>{
         if(!start) return
         const intervalId = setInterval(()=>{
@@ -35,7 +56,7 @@ const TypingLogic = ({time,setTime}:TimeDropdownProps) => {
     },[start, setTime])
 
    
-   console.log(start) 
+    
   return (
     <div>TypingLogic</div>
   )
