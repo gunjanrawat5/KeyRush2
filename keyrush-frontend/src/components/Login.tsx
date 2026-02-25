7bcf43e1c4ff769e9ac85c2407bfb1ccb26e00d3
Gunjan <gunjanrawat5@gmail.com>
2026-02-25 00:05:10 -0500

    Login/Logout img updated

--- keyrush-frontend/src/components/Login.tsx
+++ keyrush-frontend/src/components/Login.tsx
@@ -1,6 +1,7 @@
 'use client'
 import React, { useEffect, useState } from 'react'
 import { supabaseBrowser } from '../lib/supabase/client'
+import Image from 'next/image'
 
 const Login = () => {
   const [signedIn, setSignedIn] = useState(false)
@@ -35,15 +36,20 @@ const Login = () => {
     <div className="flex gap-10">
       {!signedIn ? (
         <div>
-          <div className="ml-10 mt-10 w-15 h-15 border-2">
-            <button onClick={signIn} className="w-full h-full cursor-pointer" />
+          <div className="ml-10 mt-10 w-15 h-15">
+            <button onClick={signIn} className="w-full h-full cursor-pointer">
+              <Image src = "/recycle_bin.png" alt='Login image' width={50} height={40}/>
+            </button>
           </div>
-          <p className="ml-12.5">Login</p>
+          <p className="ml-12">Login</p>
         </div>
       ) : (
         <div>
-          <div className="ml-10 mt-10 w-15 h-15 border-2">
-            <button onClick={signOut} className="w-full h-full cursor-pointer" />
+          <div className="ml-10 mt-10 w-15 h-15">
+            <button onClick={signOut} className="w-full h-full cursor-pointer">
+              <Image src = "/recycle_bin_full.png" alt='Login image' width={50} height={40}/>
+            </button>
+          
           </div>
           <p className="ml-11">Logout</p>
         </div>
